f248a1cc6ffe781936926af29f735b624deb0ec6
Zehab-bid <zehabidha152gmail.com>
2025-03-18 16:30:09 +0700

    lanjutan persegi panjang dan lingkaaran

--- file3.cpp
+++ file3.cpp
@@ -4,3 +4,22 @@ using namespace std;
 float LuasPersegiPanjang(float p,float l){
     return p * l;
 }
+
+float LuasLingkaran(float r){
+    return 3.14 * r * r;
+}
+
+int main(){
+    int pilihan;
+    float panjang, lebar, jejari;
+
+    do{
+        cout <<"=====Menu=====" << endl;
+        cout <<"1. Menghitung Luas Persegi Panjang" << endl;
+        cout <<"2. Menghitung Luas Lingkaran " << endl;
+        cout <<"3. Exit" << endl;
+        cout << "Pilihan (1/2/3) : ";
+        cin >> pilihan;
+        
+    }
+}
